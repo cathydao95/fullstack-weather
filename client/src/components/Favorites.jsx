7a94876e8d12c4fbcfd6d1ce6c566bcbe0a3a7f2
Cathy Dao <cathydao95@gmail.com>
2023-09-26 21:33:32 -0700

    add delete functionality

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -1,7 +1,27 @@
 import React, { useEffect, useState } from "react";
 import "./Favorites.css";
 
-const Favorites = ({ userFavorites }) => {
+const Favorites = ({ userFavorites, setUserFavorites }) => {
+  const deleteUserFav = async (e, id) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:8080/api/v1/users/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-type": "application/JSON",
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setUserFavorites((prevFavs) =>
+          prevFavs.filter((user) => {
+            return user.id !== id;
+          })
+        );
+      }
+    } catch (error) {}
+  };
   return (
     <div>
       <h1>Favorites</h1>
@@ -13,7 +33,7 @@ const Favorites = ({ userFavorites }) => {
               <div className="favoritesCard" key={id}>
                 <p>Name: {name}</p>
                 <p>Favorite City: {favorite_city}</p>
-                <button>Delete</button>
+                <button onClick={(e) => deleteUserFav(e, id)}>Delete</button>
                 <button>Edit</button>
               </div>
             );
