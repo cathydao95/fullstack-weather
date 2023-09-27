4301fb93d14c5a4394a8f2e79a6d7be049e06c3e
Cathy Dao <cathydao95@gmail.com>
2023-09-27 00:08:20 -0700

    add styles to favorite component

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Favorites.css";
+import EditFavorites from "./EditFavorites";
 
 const Favorites = ({ userFavorites, setUserFavorites }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingId, setEditingId] = useState();
+
   const deleteUserFav = async (e, id) => {
     e.preventDefault();
     try {
@@ -22,23 +26,51 @@ const Favorites = ({ userFavorites, setUserFavorites }) => {
       }
     } catch (error) {}
   };
+
+  const handleEditClick = (e, id) => {
+    e.preventDefault();
+    setIsEditing(true);
+    setEditingId(id);
+  };
   return (
-    <div>
+    <div className="favorites">
       <h1>Favorites</h1>
       {userFavorites && (
-        <div>
+        <div className="cardContainer">
           {userFavorites.map((user) => {
             const { id, name, favorite_city } = user;
             return (
               <div className="favoritesCard" key={id}>
-                <p>Name: {name}</p>
-                <p>Favorite City: {favorite_city}</p>
-                <button onClick={(e) => deleteUserFav(e, id)}>Delete</button>
-                <button>Edit</button>
+                <div className="cardInfoContainer">
+                  <p>Name: {name}</p>
+                  <p>Favorite City: {favorite_city}</p>
+                  <div className="eventBtnContainer">
+                    <button
+                      className="eventBtn"
+                      onClick={(e) => deleteUserFav(e, id)}
+                    >
+                      Delete
+                    </button>
+                    <button
+                      className="eventBtn"
+                      onClick={(e) => handleEditClick(e, id)}
+                    >
+                      Edit
+                    </button>
+                  </div>
+                </div>
               </div>
             );
           })}
         </div>
+      )}
+      {isEditing && (
+        <EditFavorites
+          setIsEditing={setIsEditing}
+          editingId={editingId}
+          setUserFavorites={setUserFavorites}
+          userFavorites={userFavorites}
+        />
       )}
     </div>
   );
